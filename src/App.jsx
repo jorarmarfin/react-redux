fe42cae7afea894831b2a935ef06d6a9b73ece94
Luis Mayta <luis.mayta@gmail.com>
2024-07-26 20:18:19 -0500

    simple counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment, incrementByAmount} from "./store/slices/CounterSlice.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const{contador} = useSelector(state => state.counter)
+    const dispatch = useDispatch()
+
+    const handleIncrement = () => {
+        dispatch(increment())
+    }
 
   return (
-    <>
-      <div>
+    <div className='container'>
+      <div >
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,18 +22,19 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>count is {contador}</p>
+      <div className="grid grid-cols-3 gap-2">
+        <button className='btn grid-cols-1' onClick={handleIncrement}>
+          aumentar
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+          <button onClick={()=>dispatch(decrement())} className='btn grid-cols-1'>Disminuir</button>
+          <button onClick={()=>dispatch(incrementByAmount(3))} className='btn grid-cols-1'>Aumentar por 3</button>
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
